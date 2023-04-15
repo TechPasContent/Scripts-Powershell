3aa29256bd0c6197a4d6cfbf7abf29c29942f8e8
TechPasContent <130642214+TechPasContent@users.noreply.github.com>
2023-04-15 18:16:37 +0200

    maj

--- playstation3.ps1
+++ playstation3.ps1
@@ -1,9 +1,9 @@
 
-$domainName = (Get-Content .\general.conf)[10].Split()[2]
-$Letter =(Get-Content .\general.conf)[13].Split()[2]
-$domainName = (Get-Content .\general.conf)[10].Split()[2]
-$DataFolderName = (Get-Content .\general.conf)[14].Split()[2]
-$ProfilesFolderName = (Get-Content .\general.conf)[15].Split()[2]
+$domainName = (Get-Content $PSScriptRoot\general.conf)[10].Split()[2]
+$Letter =(Get-Content $PSScriptRoot\general.conf)[13].Split()[2]
+$domainName = (Get-Content $PSScriptRoot\general.conf)[10].Split()[2]
+$DataFolderName = (Get-Content $PSScriptRoot\general.conf)[14].Split()[2]
+$ProfilesFolderName = (Get-Content $PSScriptRoot\general.conf)[15].Split()[2]
 $NewVolumeName = "Data"
 
 ################################### AUTHORIZE DHCP SERVER #############################################
@@ -17,98 +17,6 @@ Add-DhcpServerInDC -DnsName $FQDN
 Write-Host "DHCP Server successully authorized on this Domain controller" -ForegroundColor Green
 
 Write-Host "############## DHCP AUTHORISATION END ##############"
-
-############################# USERS, GRP, OU CONFIGURATION ############################################
-#######################################################################################################
-Write-Host
-Write-Host "############## AD CONFIGURATION START ##############"
-
-$DC1 = $domainName.Split(".")[1]
-$DC2 = $domainName.Split(".")[0]
-$ErrorActionPreference = 'SilentlyContinue'
-$bProtectedFromAccidentalDeletion = $false
-
-# OU configuration
-$rootPath = "DC=$DC2, DC=$DC1"
-$OUUtilisateurs = "OU=Utilisateurs, $rootPath"
-$OUDirection = "OU=Direction, $OUUtilisateurs"
-$OURD = "OU=R&D, $OUUtilisateurs"
-$OUInformatique = "OU=Informatique, $OUUtilisateurs"
-$OUCommunication = "OU=Communication, $OUUtilisateurs"
-$OUComptabilite = "OU=Comptabilite, $OUUtilisateurs"
-$OUPostesClients = "OU='Postes CLients', $rootPath"
-$OUGroupes = "OU=Groupes, $rootPath"
-
-# Users @(OU, Name, SamAccountName, Groups) 
-$Users = @(
-    @($OUComptabilite, "Michel BONNET", "bonnetm", @("GG_Comptabilite")),
-    @($OUComptabilite, "Sarah MOULA", "moulas", @("GG_Comptabilite")),
-    @($OUCommunication, "Alexis BAVARD", "bavarda", @("GG_Communication")),
-    @($OUCommunication, "Aurelie PUB", "puba", @("GG_Communication")),
-    @($OUInformatique, "Jean-Michel GEEK", "geekj", @("GG_Informatique")),
-    @($OUInformatique, "Abdelhalim MMORPG", "mmorpga", @("GG_Informatique")),
-    @($OURD, "Bob RECHERCHE", "rechercheb", @("GG_RD")),
-    @($OURD, "Boubakar DESTRUCTION", "destructionb", @("GG_RD")),
-    @($OUDirection, "Richard DIRIGEUR", "dirigeurr", @("GG_Direction")),
-    @($OUDirection, "Gollum ACCOLYTE", "accolyteg",@("GG_Direction"))
-    )
-# Groups
-$Groups = @("GG_Paris", "GG_LeHavre", "GG_Toulouse", "GG_Bordeaux",`
-				"GG_Comptabilite", "GG_Communication", "GG_Informatique", "GG_RD", "GG_Direction", `
-                "GDL_Direction_Partage", "GDL_Comptabilite_Partage", "GDL_Communication_Partage", `
-                "GDL_Informatique_Partage", "GDL_RD_Partage", `
-								"GDL_ProfilsItinerants")
-
-# User configuration
-$AccountPassword = Read-Host -AsSecureString "Users password. One for all !"
-$ProfilePath = "\\$FQDN\Profils$\%username%"
-
-# Add OU
-New-ADOrganizationalUnit -Path $rootPath -Name "Utilisateurs" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Direction" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "R&D" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Informatique" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Communication" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Comptabilité" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $rootPath -Name "Postes CLients" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-New-ADOrganizationalUnit -Path $rootPath -Name "Groupes" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
-Write-Host "OU configured successfully." -ForegroundColor Green
-
-# Add Groups
-foreach ($g in $GG){
-		New-ADGroup -GroupScope Global -GroupCategory Security -Name $g -Path $OUGroupes
-}
-Write-Host "Groups configured successfully." -ForegroundColor Green
-
-# Add Users
-foreach ($u in $Users){
-    New-ADUser -Path $u[0] -Name $u[1] -SamAccountName $u[2] `
-        -AccountPassword (ConvertTo-SecureString -String $AccountPassword -AsPlainText -Force) `
-        -Enabled $true -CannotChangePassword $true -PasswordNeverExpires $true `
-        -ProfilePath $ProfilePath
-    # Set users members of groups
-    foreach ($g in $u[3]){
-        Add-ADGroupMember -Identity $g -Members $u[2]
-    }        
-}
-Write-Host "Users configured successfully." -ForegroundColor Green
-Write-Host "Profile path of users : $ProfilePath"
-
-# Add GG in GDL
-Add-ADGroupMember -Identity GDL_Direction_Partage -Members GG_Direction
-Add-ADGroupMember -Identity GDL_Comptabilite_Partage -Members GG_Comptabilite
-Add-ADGroupMember -Identity GDL_Communication_Partage -Members GG_Communication
-Add-ADGroupMember -Identity GDL_RD_Partage -Members GG_RD
-Add-ADGroupMember -Identity GDL_Informatique_Partage -Members GG_Informatique
-Add-ADGroupMember -Identity GDL_ProfilsItinerants -Members GG_Comptabilite, GG_Communication, GG_Informatique, GG_RD, GG_Direction
-Write-Host "GG placed in GDL successfully." -ForegroundColor Green
-
-
-# New joined computers redirection
-C:\Windows\System32\redircmp.exe "OU=Postes Clients, DC=isec, DC=local" | Out-Null
-Write-Host "New computer will automatically be placed under [$OUPostesClients]."
-
-Write-Host "############## AD CONFIGURATION END ##############"
 
 
 ######################### CHECK VOLUME THAT STORE DATA OF AD USERS  ###################################
@@ -185,6 +93,7 @@ if ($Partition -or $Volume) {
 
 Write-Host "############## CHECK VOLUME CONFIGURATION END ##############"
 
+
 ################################## SHARE CONFIGURATION ################################################
 #######################################################################################################
 
@@ -196,15 +105,113 @@ $ProfilesFolderPath = $Letter + ":\"
 $DataFolderPath = $Letter + ":\" 
 
 # Folders creation
-$ProfileFolder = New-Item -ItemType Directory -Name $ProfilesFolderName -Path $ProfilesFolderPath
+New-Item -ItemType Directory -Name $ProfilesFolderName -Path $ProfilesFolderPath
+$ProfilesFolder = $ProfilesFolderPath + $ProfilesFolderName
 Write-Host "$ProfilesFolderPath$ProfilesFolderName created"
 
 $DataFolder = New-Item -ItemType Directory -Name $DataFolderName -Path $DataFolderPath 
 Write-Host "$DataFolderPath$DataFolderName created"
 
+New-SmbShare -Path $ProfilesFolder -FullAccess "Administrateurs" -Name $ProfilesFolderName -CachingMode None
 
 
 Write-Host "############## SHARE CONFIGURATION END ##############"
 
 
 pause
+
+############################# USERS, GRP, OU CONFIGURATION ############################################
+#######################################################################################################
+Write-Host
+Write-Host "############## AD CONFIGURATION START ##############"
+
+$DC1 = $domainName.Split(".")[1]
+$DC2 = $domainName.Split(".")[0]
+$ErrorActionPreference = 'SilentlyContinue'
+$bProtectedFromAccidentalDeletion = $false
+
+# OU configuration
+$rootPath = "DC=$DC2, DC=$DC1"
+$OUUtilisateurs = "OU=Utilisateurs, $rootPath"
+$OUDirection = "OU=Direction, $OUUtilisateurs"
+$OURD = "OU=R&D, $OUUtilisateurs"
+$OUInformatique = "OU=Informatique, $OUUtilisateurs"
+$OUCommunication = "OU=Communication, $OUUtilisateurs"
+$OUComptabilite = "OU=Comptabilite, $OUUtilisateurs"
+$OUPostesClients = "OU='Postes CLients', $rootPath"
+$OUGroupes = "OU=Groupes, $rootPath"
+
+# Users @(OU, Name, SamAccountName, Groups) 
+$Users = @(
+    @($OUComptabilite, "Michel BONNET", "bonnetm", @("GG_Comptabilite")),
+    @($OUComptabilite, "Sarah MOULA", "moulas", @("GG_Comptabilite")),
+    @($OUCommunication, "Alexis BAVARD", "bavarda", @("GG_Communication")),
+    @($OUCommunication, "Aurelie PUB", "puba", @("GG_Communication")),
+    @($OUInformatique, "Jean-Michel GEEK", "geekj", @("GG_Informatique")),
+    @($OUInformatique, "Abdelhalim MMORPG", "mmorpga", @("GG_Informatique")),
+    @($OURD, "Bob RECHERCHE", "rechercheb", @("GG_RD")),
+    @($OURD, "Boubakar DESTRUCTION", "destructionb", @("GG_RD")),
+    @($OUDirection, "Richard DIRIGEUR", "dirigeurr", @("GG_Direction")),
+    @($OUDirection, "Gollum ACCOLYTE", "accolyteg",@("GG_Direction"))
+    )
+# Groups
+$Groups = @("GG_Paris", "GG_LeHavre", "GG_Toulouse", "GG_Bordeaux",`
+				"GG_Comptabilite", "GG_Communication", "GG_Informatique", "GG_RD", "GG_Direction", `
+                "GDL_Direction_Partage", "GDL_Comptabilite_Partage", "GDL_Communication_Partage", `
+                "GDL_Informatique_Partage", "GDL_RD_Partage", `
+								"GDL_ProfilsItinerants")
+
+# User configuration
+$AccountPassword = Read-Host -AsSecureString "Users password. One for all !"
+$ProfilePath = "\\$FQDN\$ProfilesSharedPath\%username%"
+
+# Add OU
+New-ADOrganizationalUnit -Path $rootPath -Name "Utilisateurs" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Direction" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "R&D" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Informatique" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Communication" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $OUUtilisateurs -Name "Comptabilité" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $rootPath -Name "Postes CLients" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+New-ADOrganizationalUnit -Path $rootPath -Name "Groupes" -ProtectedFromAccidentalDeletion $bProtectedFromAccidentalDeletion
+Write-Host "OU configured successfully." -ForegroundColor Green
+
+# Add Groups
+foreach ($g in $Groups){
+		New-ADGroup -GroupScope Global -GroupCategory Security -Name $g -Path $OUGroupes
+}
+Write-Host "Groups configured successfully." -ForegroundColor Green
+
+# Add Users
+foreach ($u in $Users){
+    New-ADUser -Path $u[0] -Name $u[1] -SamAccountName $u[2] `
+        -AccountPassword (ConvertTo-SecureString -String $AccountPassword -AsPlainText -Force) `
+        -Enabled $true -CannotChangePassword $true -PasswordNeverExpires $true `
+        -ProfilePath $ProfilePath
+    # Set users members of groups
+    foreach ($g in $u[3]){
+        Add-ADGroupMember -Identity $g -Members $u[2]
+    }        
+}
+Write-Host "Users configured successfully." -ForegroundColor Green
+Write-Host "Profile path of users : $ProfilePath"
+
+# Add GG in GDL
+Add-ADGroupMember -Identity GDL_Direction_Partage -Members GG_Direction
+Add-ADGroupMember -Identity GDL_Comptabilite_Partage -Members GG_Comptabilite
+Add-ADGroupMember -Identity GDL_Communication_Partage -Members GG_Communication
+Add-ADGroupMember -Identity GDL_RD_Partage -Members GG_RD
+Add-ADGroupMember -Identity GDL_Informatique_Partage -Members GG_Informatique
+Add-ADGroupMember -Identity GDL_ProfilsItinerants -Members GG_Comptabilite, GG_Communication, GG_Informatique, GG_RD, GG_Direction
+Write-Host "GG placed in GDL successfully." -ForegroundColor Green
+
+
+# New joined computers redirection
+C:\Windows\System32\redircmp.exe "OU=Postes Clients, DC=isec, DC=local" | Out-Null
+Write-Host "New computer will automatically be placed under [$OUPostesClients]."
+
+Write-Host "############## AD CONFIGURATION END ##############"
+
+
+
+
