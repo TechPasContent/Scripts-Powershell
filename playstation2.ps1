79941ee0a066209b03c1e28a72e15ac50eec28d6
TechPasContent <130642214+TechPasContent@users.noreply.github.com>
2023-04-16 15:19:05 +0200

    Fonctionnel pour plusieurs serveurs

--- playstation2.ps1
+++ playstation2.ps1
@@ -10,12 +10,13 @@ $dhcpSubnetMask = (Get-Content .\general.conf)[9].Split()[2]
 $domainName = (Get-Content .\general.conf)[10].Split()[2]
 $domainNetBIOSName = (Get-Content .\general.conf)[11].Split()[2]
 $bInstallDNS = (Get-Content .\general.conf)[12].Split()[2]
-if ($bInstallDNS -ieq "false") {
-    $bInstallDNS = $false
-} else {
-    $bInstallDNS = $true
-}
-
+if ($bInstallDNS -ieq "false") { $bInstallDNS = $false} else { $bInstallDNS = $true }
+$bInstallDHCP = (Get-Content .\general.conf)[16].Split()[2]
+if ($bInstallDHCP -ieq "false") { $bInstallDHCP = $false} else { $bInstallDHCP = $true }
+$bInstallAD = (Get-Content .\general.conf)[17].Split()[2]
+if ($bInstallAD -ieq "false") { $bInstallAD = $false} else { $bInstallAD = $true }
+$bInstallForest = (Get-Content .\general.conf)[18].Split()[2]
+if ($bInstallForest -ieq "false") { $bInstallForest = $false} else { $bInstallForest = $true }
 
 #################################### INTERFACE CONFIGURATION ##########################################
 #######################################################################################################
@@ -66,64 +67,69 @@ Write-Host "############## IPv4 END ##############"
 ########################################## DHCP SERVER ################################################
 #######################################################################################################
 
-Write-Host
-Write-Host "############## DHCP START ##############"
+if ($bInstallDHCP) {
 
-# Install DHCP Server role
-if ((Get-WindowsFeature -Name DHCP).installed) {
-    Write-Host "DHCP Server is already installed."    
-} else {
-    Install-WindowsFeature -Name DHCP -IncludeManagementTools
-    Write-Host "DHCP Role added to the server successfully" -ForegroundColor Green
+    Write-Host
+    Write-Host "############## DHCP START ##############"
+
+    # Install DHCP Server role
+    if ((Get-WindowsFeature -Name DHCP).installed) {
+        Write-Host "DHCP Server is already installed."    
+    } else {
+        Install-WindowsFeature -Name DHCP -IncludeManagementTools
+        Write-Host "DHCP Role added to the server successfully" -ForegroundColor Green
+    }
+
+    # Configure DHCP settings
+    $dhcpServer = "localhost"
+    $dhcpScopeName = "Main DHCP pool"
+
+
+    # Check if the DHCP scope already exists
+    $existingScopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer | Where-Object { $_.Name -like $dhcpScopeName }
+    if ($existingScopes) {
+        Write-Host "DHCP scope '$dhcpScopeName' already exists." -ForegroundColor Yellow
+    } else {
+        # Add DHCP scope
+        Add-DhcpServerv4Scope -ComputerName $dhcpServer -Name $dhcpScopeName -StartRange $dhcpScopeStart -EndRange $dhcpScopeEnd -SubnetMask $dhcpSubnetMask
+        Write-Host "Scope [$dhcpScopeStart - $dhcpScopeEnd] added successfully." -ForegroundColor Green
+
+        # Get the IP address of the DHCP scope
+        $dhcpScope = Get-DhcpServerv4Scope -ComputerName $dhcpServer | Where-Object { $_.Name -eq $dhcpScopeName }
+        $dhcpScopeId = $dhcpScope.ScopeId
+
+        # Set DHCP scope options
+        Set-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScopeId -OptionId 6 -Value $DNS1, $DNS2  -Force  # DNS Servers
+        Set-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScopeId -OptionId 51 -Value 345600      # Lease Time (4 days)
+        Write-Host "Lease time set to 4 days"
+        Write-Host "DNS added : $DNS1, $DNS2"
+    }
+
+    Write-Host "############## DHCP END ##############"
 }
-
-# Configure DHCP settings
-$dhcpServer = "localhost"
-$dhcpScopeName = "Main DHCP pool"
-
-
-# Check if the DHCP scope already exists
-$existingScopes = Get-DhcpServerv4Scope -ComputerName $dhcpServer | Where-Object { $_.Name -like $dhcpScopeName }
-if ($existingScopes) {
-    Write-Host "DHCP scope '$dhcpScopeName' already exists." -ForegroundColor Yellow
-} else {
-    # Add DHCP scope
-    Add-DhcpServerv4Scope -ComputerName $dhcpServer -Name $dhcpScopeName -StartRange $dhcpScopeStart -EndRange $dhcpScopeEnd -SubnetMask $dhcpSubnetMask
-    Write-Host "Scope [$dhcpScopeStart - $dhcpScopeEnd] added successfully." -ForegroundColor Green
-
-    # Get the IP address of the DHCP scope
-    $dhcpScope = Get-DhcpServerv4Scope -ComputerName $dhcpServer | Where-Object { $_.Name -eq $dhcpScopeName }
-    $dhcpScopeId = $dhcpScope.ScopeId
-
-    # Set DHCP scope options
-    Set-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScopeId -OptionId 6 -Value $DNS1, $DNS2  -Force  # DNS Servers
-    Set-DhcpServerv4OptionValue -ComputerName $dhcpServer -ScopeId $dhcpScopeId -OptionId 51 -Value 345600      # Lease Time (4 days)
-    Write-Host "Lease time set to 4 days"
-    Write-Host "DNS added : $DNS1, $DNS2"
-}
-
-Write-Host "############## DHCP END ##############"
 
 ########################################## AD SERVER ##################################################
 #######################################################################################################
 
-Write-Host
-Write-Host "############## AD START ##############"
+if ($bInstallAD) {
+    Write-Host
+    Write-Host "############## AD START ##############"
 
-# Install Active Directory Feature
-if ((Get-WindowsFeature -Name AD-Domain-Services).installed) {
-    Write-Host "AD-DS Server is already installed."    
-} else {
-    Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
-    Write-Host "AD-DS Role added to the server successfully" -ForegroundColor Green
+    # Install Active Directory Feature
+    if ((Get-WindowsFeature -Name AD-Domain-Services).installed) {
+        Write-Host "AD-DS Server is already installed."    
+    } else {
+        Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+        Write-Host "AD-DS Role added to the server successfully" -ForegroundColor Green
+        if ($bInstallForest) {
+            # Create a new forest
+            Install-ADDSForest -DomainName $domainName -DomainNetBIOSName $domainNetBIOSName -SafeModeAdministratorPassword $adminPassword -Force -InstallDNS:$bInstallDNS
+            Write-Host "New forest created successfully." -ForegroundColor Green
+        }
+        # Restart the Server
+        Restart-Computer -Confirm
+    }
 
-    # Create a new forest
-    Install-ADDSForest -DomainName $domainName -DomainNetBIOSName $domainNetBIOSName -SafeModeAdministratorPassword $adminPassword -Force -InstallDNS:$bInstallDNS
-    Write-Host "New forest created successfully." -ForegroundColor Green
-
-    # Restart the Server
-    Restart-Computer -Confirm
+    Write-Host "############## AD END ##############"
 }
-
-Write-Host "############## AD END ##############"
 pause
